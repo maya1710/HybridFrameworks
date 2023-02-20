1d6a27cb486db649ff41e0385260cbd37b0130bc
@maya1710 <mahamaya.lenka@gmail.com>
2023-02-20 23:58:55 +0100

    added createcourse

---
+++ src/test/java/testcases/CreateCourse.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+
+public class CreateCourse  extends BaseClass{
+
+	
+	@Test
+	public void createCourse()
+	{
+		
+		
+		
+	}
+
+}
